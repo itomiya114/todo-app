733ff7dcd0056d427068219afce28fc929739b0c
itomiya114 <116330970+itomiya114@users.noreply.github.com>
2023-05-08 20:32:43 +0900

    done

--- script.js
+++ script.js
@@ -1,144 +1,146 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+    TODO_ITEM: 'todo-container',
+    TODO_CHECKBOX: 'todo-checkbox',
+    TODO_TEXT: 'todo-text',
+    TODO_DELETE: 'todo-delete',
 }
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 let locallist = JSON.parse(localStorage.getItem("todo"));
-let num = 0;
 if (!locallist) {
-  locallist = [];
+    locallist = {};
 }
-locallist.forEach(todoItem => {
 
-  newTodo(todoItem.todo == null ? "untitled" : todoItem.todo, todoItem.checked);
-});
+for (let key in locallist) {
+    newTodo(key == null ? "untitled" : key, locallist[key].checked);
+
+}
 function newTodo(todoText = null, checked = false) {
-  if (todoText == null) {
-    // get todoText from user
-    todoText = prompt("Add a todo!");
-    if (!todoText) {
-      return;
+    if (todoText == null) {
+        // get todoText from user
+        todoText = prompt("Add a todo!");
+        if (!todoText) {
+            return;
+        }
+        locallist[todoText] = { checked: false };
+        localStorage.setItem("todo", JSON.stringify(locallist));
     }
-    locallist.push({ todo: todoText, checked: false });
-    localStorage.setItem("todo", JSON.stringify(locallist));
-  }
 
-  // create a todoElement with inputted todoText
-  todoElement = makeTodo(todoText);
-  if (checked) {
-    todoElement.getElementsByClassName(classNames.TODO_CHECKBOX)[0].checked = checked;
-  }
-  todoElement.getElementsByClassName(classNames.TODO_CHECKBOX)[0].index = num;
-  num++;
-  // append todoElement to DOM list element
-  render(todoElement, list)
-
-  // count newtodo
-  countNewTodo(itemCountSpan)
-
-  // count unchecked todos
-  if(!checked){
-  countUncheckedTodo(uncheckedCountSpan)
-  }
-  // select todoElement's checkbox from the DOM
-  const checkbox = todoElement.children[todoElement.children.length-2];
-
-  // if checkbox checked decrement unchecked todos count by one otherwise do the opposite
-  checkbox.addEventListener("click", function (event) {
-    locallist[event.target.index].checked = event.target.checked;
-    localStorage.setItem("todo", JSON.stringify(locallist));
-    if (event.target.checked) {
-      countCheckedTodo(uncheckedCountSpan)
-    } else {
-      countUncheckedTodo(uncheckedCountSpan)
+    // create a todoElement with inputted todoText
+    todoElement = makeTodo(todoText);
+    if (checked) {
+        todoElement.getElementsByClassName(classNames.TODO_CHECKBOX)[0].checked = checked;
     }
-  })
+    // append todoElement to DOM list element
+    render(todoElement, list)
+
+    // count newtodo
+    count(itemCountSpan, 1);
+
+    // count unchecked todos
+    if (!checked) {
+        count(uncheckedCountSpan, 1);
+    }
+    // select todoElement's checkbox from the DOM
+    const checkbox = todoElement.children[1];
+
+    // if checkbox checked decrement unchecked todos count by one otherwise do the opposite
+    checkbox.addEventListener("click", function (event) {
+        let key = event.target.parentElement.children[0].innerHTML;
+
+        if (locallist[key]) {
+            locallist[key].checked = event.target.checked;
+        }
+        localStorage.setItem("todo", JSON.stringify(locallist));
+        count(uncheckedCountSpan, event.target.checked ? -1 : 1);
+    })
 
 }
 
-const deleteTodo=(index)=>{
-list.children[index].remove();
-for(let i=index+1;i<list.length;i++){
+function deleteTodo(e) {
+    console.log(e);
+    if (!e.target.parentElement.children[1].checked) {
+        count(uncheckedCountSpan, -1);
+    }
+    count(itemCountSpan, -1);
 
-}
-localStorage.
+    delete locallist[event.target.parentElement.children[0].innerHTML];
+    localStorage.setItem("todo", JSON.stringify(locallist));
+    e.target.parentElement.remove();
 }
 // create todoElements
 function makeTodo(todoText) {
-  // create li
-  const li = document.createElement("li")
+    // create li
+    const li = document.createElement("li")
 
-  addStyle(classNames.TODO_ITEM, li)
+    addStyle(classNames.TODO_ITEM, li)
 
-  // create span
-  const span = document.createElement("span")
+    // create span
+    const span = document.createElement("span")
 
-  addStyle(classNames.TODO_TEXT, span)
+    addStyle(classNames.TODO_TEXT, span)
 
-  // create input and set it's type to checkbox
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox")
+    // create input and set it's type to checkbox
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox")
 
-  addStyle(classNames.TODO_CHECKBOX, checkbox)
-  const button = document.createElement("button");
-  button.innerHTML = "delete";
+    addStyle(classNames.TODO_CHECKBOX, checkbox)
+    const button = document.createElement("button");
+    button.innerHTML = "delete";
 
-  button.addEventListener("click",()=>{alert();});
-  // add text to span
-  span.textContent = todoText
+    button.addEventListener("click", deleteTodo);
+    // add text to span
+    span.textContent = todoText
 
-  // append span to li
-  render(span, li)
+    // append span to li
+    render(span, li)
 
-  // appent checkbox to li
-  render(checkbox, li)
-  render(button, li);
-  // return todoElement
-  return li
+    // appent checkbox to li
+    render(checkbox, li)
+    render(button, li);
+    // return todoElement
+    return li
 
 }
 
 
 // append elements to the DOM
 function render(element, destination) {
-  destination.appendChild(element)
+    destination.appendChild(element)
 }
 
 
 // count total todos
 function countNewTodo(element) {
-  count(element)
+    count(element)
 }
 
 
 // increase unchecked todos count everytime new tod0 added
 function countUncheckedTodo(element) {
-  count(element)
+    count(element)
 }
 
 // factor out common code for countNewTodo and countUncheckedTodo functions
-function count(element) {
-  let count = Number(element.innerHTML)
-  count++
-  element.innerHTML = count.toString()
+function count(element, num) {
+    let count = Number(element.innerHTML)
+    count += num;
+    element.innerHTML = count.toString()
 }
 
 
 // decrease unchecked todos count if certain element is checked
 function countCheckedTodo(element) {
-  let count = Number(element.innerHTML)
-  count--
-  element.innerHTML = count.toString()
+    let count = Number(element.innerHTML)
+    count--
+    element.innerHTML = count.toString()
 }
 
 // add css to html elements
 function addStyle(style, element) {
-  element.setAttribute("class", style)
+    element.setAttribute("class", style)
 }
 
 

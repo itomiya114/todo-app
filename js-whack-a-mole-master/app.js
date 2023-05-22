38fccbe3e462e72c25041f6e7881683fbff317ce
114 <116330970+itomiya114@users.noreply.github.com>
2023-05-22 20:10:51 +0900

    Update app.js

--- js-whack-a-mole-master/app.js
+++ js-whack-a-mole-master/app.js
@@ -1,35 +1,37 @@
 const SQUARE = document.querySelectorAll('.square');
 const MOLE = document.querySelectorAll('.mole');
 const TIMELEFT = document.querySelector('#time-left');
+const SQUARE_LENGTH = SQUARE.length;
 let score = document.querySelector('#score');
-
 let result = 0;
 let currentTime = TIMELEFT.textContent;
-let prevIndex = -1;
 let hitPosition = null;
+SQUARE.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if (id.id === hitPosition) {
+            result++;
+            score.textContent = result;
+             removeMole(id.id);
+        }
+    })
+})
+let timerMoleId = setInterval(randomSquare, 1000);
+let timerId = setInterval(countDown, 1000);
+function removeMole(id){
+    document.getElementById(id).classList.remove("mole");
+    hitPosition = null;
+}
 function randomSquare() {
-    if (prevIndex >= 0) {
-        SQUARE[prevIndex].classList.remove("mole");
+    if (hitPosition) {
+        removeMole(hitPosition);
     }
-    prevIndex = Math.floor(Math.random() * 9);
-    let randomPosition = SQUARE[prevIndex];
+    let randomPosition = SQUARE[Math.floor(Math.random() *  SQUARE_LENGTH)];
     randomPosition.classList.add('mole');
 
     //assign the if the randomPosition to hitPosition to use later
     hitPosition = randomPosition.id;
     
 }
-
-SQUARE.forEach(id => {
-    id.addEventListener('mouseup', () => {
-        if (id.id === hitPosition) {
-            result++;
-            score.textContent = result;
-            id.classList.remove("mole");
-            hitPosition = null;
-        }
-    })
-})
 
 function countDown() {
     currentTime--;
@@ -38,9 +40,11 @@ function countDown() {
     if (currentTime === 0) {
         clearInterval(timerMoleId);
         clearInterval(timerId);
+        if (hitPosition) {
+            removeMole(hitPosition);
+        }
         alert('GAME OVER! your final score is' + result);
     }
 }
 
-let timerMoleId = setInterval(randomSquare, 1000);;
-let timerId = setInterval(countDown, 1000);
+
